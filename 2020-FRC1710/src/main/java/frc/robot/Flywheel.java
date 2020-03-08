272287cf88449c4022022f046b12d254d8edf5fe
Cyrus Duong <cyrusduong@gmail.com>
2020-03-08 09:19:21 -0500

    Cleanup Flywheel.java

--- 2020-FRC1710/src/main/java/frc/robot/Flywheel.java
+++ 2020-FRC1710/src/main/java/frc/robot/Flywheel.java
@@ -6,25 +6,16 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
-import com.kauailabs.navx.frc.AHRS;
+
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DigitalSource;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.GenericHID.Hand;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.PowerDistributionPanel;
 
 /**
- * Add your docs here.
+ * Controls the flywheel on the shooter. Personally would name this Shooter.java
  */
 public class Flywheel {
     public static CANSparkMax m_flyOne, m_flyTwo, m_feeder;
@@ -32,49 +23,57 @@ public class Flywheel {
     public static double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM;
     public static MiniPID flywheelPID, feederPID;
     public static double flywheelSpeed, feederSpeed;
-    public static void initShooter(){
+    
+    public static void initShooter() {
+        // Init motors
         m_flyOne = new CANSparkMax(8, MotorType.kBrushless);
         m_flyTwo = new CANSparkMax(9, MotorType.kBrushless);
         m_feeder = new CANSparkMax(11, MotorType.kBrushless);
-        m_flyOne.restoreFactoryDefaults();
-        m_flyTwo.restoreFactoryDefaults();
-        m_feeder.restoreFactoryDefaults();
+
+        // Group and inverse secondary flywheel motor to primary flywheel motor.
+        m_flyOne.follow(m_flyTwo, true);
+
+        // Get encoders 
         e_flyOne = m_flyOne.getEncoder();
         e_flyTwo = m_flyTwo.getEncoder();
         e_feeder = m_feeder.getEncoder();
+        
         m_flyOne.setIdleMode(IdleMode.kCoast);
         m_flyTwo.setIdleMode(IdleMode.kCoast);
         m_feeder.setIdleMode(IdleMode.kCoast);
+        
         kP = .0002; 
-        kI = .000000000001;
+        kI = .000000000001; // This is really small. Wonder looks like it's to compensate for steady-state error?
+        // Review https://www.ni.com/en-us/innovations/white-papers/06/pid-theory-explained.html
         kD = 0; 
         kIz = 0; 
         kFF = 0; 
         kMaxOutput = 1; 
         kMinOutput = -1;
         maxRPM = 5700;
+        
         flywheelPID = new MiniPID(kP,kI,kD); //values need to be changed
         feederPID = new MiniPID(kP,kI,kD); //values need to be changed
     }
-    public static void setShooterSpeeds(double flywheelRPM, double feederRPM, boolean On){
-        
-        if(On == true){
+    
+    public static void setShooterSpeeds(double flywheelRPM, double feederRPM, boolean isOn) {
+        if (isOn) {
             flywheelSpeed = flywheelPID.getOutput(-e_flyOne.getVelocity(), flywheelRPM);
             feederSpeed = feederPID.getOutput(e_feeder.getVelocity(), -feederRPM);
-            m_flyOne.set(-flywheelSpeed);
-           // SmartDashboard.putNumber("output", flywheelSpeed);
+            
             m_flyTwo.set(flywheelSpeed);
             m_feeder.set(feederSpeed);
         } else {
             flywheelPID.reset();
             feederPID.reset();
+            
             flywheelSpeed = flywheelPID.getOutput(e_flyOne.getVelocity(), 0);
-           // SmartDashboard.putNumber("output", flywheelSpeed);
             feederSpeed = feederPID.getOutput(e_feeder.getVelocity(), 0);
-            m_flyOne.set(0);
+            
             m_flyTwo.set(0);
             m_feeder.set(0);
         }
+
         SmartDashboard.putNumber("pid-out", flywheelSpeed);
         SmartDashboard.putNumber("goal", flywheelRPM);
         SmartDashboard.putNumber("actual", e_flyOne.getVelocity());
@@ -83,6 +82,7 @@ public class Flywheel {
     public static double getFlySpeed(){
         return e_flyOne.getVelocity();
     }
+
     public static double getFeedSpeed(){
         return e_feeder.getVelocity();
     }
